1d95ceb3164ac5c4ae46199573e231bd4fe6c582
Ariq <ariqchowdhury@gmail.com>
2025-08-04 19:49:53 -0400

    Add license headers

--- protocol/benches/counter.rs
+++ protocol/benches/counter.rs
@@ -1,3 +1,18 @@
+// Copyright (C) 2025 Category Labs, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 use divan::Bencher;
 use protocol::{compute_length, serialize_to_buf, Counter, Labels};
 use rkyv::api::high::to_bytes_in;
